7388e2622b9d4a2d8270bf77ce1048815af167a3
Pham Dang Quang <phamquang040102@gmail.com>
2025-11-03 12:01:27 +0700

    redux,navlink,link,style css by navlink routers provider

--- b5-react-route-dom/b5-router/src/App.jsx
+++ b5-react-route-dom/b5-router/src/App.jsx
@@ -1,30 +1,51 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, NavLink } from "react-router-dom";
 import Home from "./home";
 import Contact from "./contact";
 import News from "./news";
 import LoginFormBackend from "./FormLoginAPi";
+import { Counter } from "./Counter";
 function App() {
+  // link  , nav link hook react
+  //Styles
+
+  const navChecked = (isActive) => {
+    console.log("Is active : ", isActive);
+    return isActive ? "active" : "deactive";
+  };
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <Link href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        </Link>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      <nav>
+        <NavLink to="/home" className={({ isActive }) => navChecked(isActive)}>
+          Home
+        </NavLink>
+        | <NavLink to="/contact">Contact</NavLink>
+      </nav>
       <h2>
-        {" "}
-        <a href="/home">Homes</a>{" "}
+        <Link
+          to={{
+            pathname: "/home",
+          }}
+        >
+          Home
+        </Link>
       </h2>
       <h2>
-        {" "}
-        <a href="/contact">Contact</a>
+        {/* nav link có thêm đánh dấu trang */}
+        <NavLink to="/contact">Contact</NavLink>
       </h2>
       <h2>
         <a href="/news">News</a>
@@ -32,11 +53,15 @@ function App() {
       <h2>
         <a href="/form">Create Form</a>
       </h2>
-      <Routes>
+      <h2>
+        <a href="/redux">Redux</a>
+      </h2>
+      <Routes path="/admin">
         <Route path="/home" element={<Home />} />
-        <Route path="/contact" element={<News />} />
-        <Route path="/news" element={<Contact />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/form" element={<LoginFormBackend />} />
+        <Route path="/redux" element={<Counter />} />
       </Routes>
     </>
   );
